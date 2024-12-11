7d80baeebafa3b98616268488ac466110008f93b
muffindud <ccatlabuga2002@gmail.com>
2024-12-11 16:39:32 +0200

    add enemy walking animation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,9 +10,45 @@ public class EnemyController : MonoBehaviour
     [Range(0.0f, 2.0f)]
     public float moveSpeed = 1.0f;
 
+    public SpriteRenderer sr;
+
+    const float animationSpeed = 5.0f;
+
+    public Sprite frontStaticSprite;
+    public Sprite[] frontWalkSprites;
+    public Sprite backStaticSprite;
+    public Sprite[] backWalkSprites;
+    public Sprite sideStaticSprite; // default is right
+    public Sprite[] sideWalkSprites;
+
+    Sprite getSprite(Vector2 direction, bool isWalking)
+    {
+        sr.flipX = false;
+        if (direction.y > 0)
+        {
+            return isWalking ? backWalkSprites[(int)(Time.time * animationSpeed) % backWalkSprites.Length] : backStaticSprite;
+        }
+        else if (direction.y < 0)
+        {
+            return isWalking ? frontWalkSprites[(int)(Time.time * animationSpeed) % frontWalkSprites.Length] : frontStaticSprite;
+        }
+        else if (direction.x > 0)
+        {
+            return isWalking ? sideWalkSprites[(int)(Time.time * animationSpeed) % sideWalkSprites.Length] : sideStaticSprite;
+        }
+        else if (direction.x < 0)
+        {
+            sr.flipX = true;
+            return isWalking ? sideWalkSprites[(int)(Time.time * animationSpeed) % sideWalkSprites.Length] : sideStaticSprite;
+        }
+
+        return frontStaticSprite;
+    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -20,5 +56,7 @@ public class EnemyController : MonoBehaviour
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
         rb.velocity = direction * moveSpeed;
+
+        sr.sprite = getSprite(direction, direction.magnitude > 0);
     }
 }
