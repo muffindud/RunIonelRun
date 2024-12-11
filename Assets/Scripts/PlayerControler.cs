16fc7b61fdff5b661041e594d421b26df0ab0dab
muffindud <ccatlabuga2002@gmail.com>
2024-12-11 16:34:57 +0200

    add player movement animation

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -7,20 +7,54 @@ public class PlayerControler : MonoBehaviour
     public float moveSpeed;
     Vector2 moveInput;
     Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    const float animationSpeed = 7.5f;
+
+    public Sprite frontStaticSprite;
+    public Sprite[] frontWalkSprites;
+    public Sprite backStaticSprite;
+    public Sprite[] backWalkSprites;
+    public Sprite sideStaticSprite; // default is right
+    public Sprite[] sideWalkSprites;
+
+    Sprite getSprite(Vector2 direction, bool isWalking)
+    {
+        sr.flipX = false;
+        if (direction.y > 0)
+        {
+            return isWalking ? backWalkSprites[(int)(Time.time * animationSpeed) % backWalkSprites.Length] : backStaticSprite;
+        }
+        else if (direction.y < 0)
+        {
+            return isWalking ? frontWalkSprites[(int)(Time.time * animationSpeed) % frontWalkSprites.Length] : frontStaticSprite;
+        }
+        else if (direction.x > 0)
+        {
+            return isWalking ? sideWalkSprites[(int)(Time.time * animationSpeed) % sideWalkSprites.Length] : sideStaticSprite;
+        }
+        else if (direction.x < 0)
+        {
+            sr.flipX = true;
+            return isWalking ? sideWalkSprites[(int)(Time.time * animationSpeed) % sideWalkSprites.Length] : sideStaticSprite;
+        }
+
+        return frontStaticSprite;
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        sr.sprite = frontStaticSprite;
     }
 
     void Update()
     {
         moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         moveInput.Normalize();
-        // var pos = transform.position;
-        // pos.x += moveInput.x * moveSpeed * Time.deltaTime;
-        // pos.y += moveInput.y * moveSpeed * Time.deltaTime;
-        // transform.position = pos;
         rb.velocity = moveInput * moveSpeed;
+        sr.sprite = getSprite(moveInput, moveInput.magnitude > 0);
     }
 }
