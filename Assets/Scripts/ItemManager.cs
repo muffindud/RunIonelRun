fe01df99cdf3b7928b2bf4856659c3c03e252492
muffindud <ccatlabuga2002@gmail.com>
2025-01-17 14:53:31 +0200

    add key spawns

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -6,24 +6,39 @@ public class ItemManager : MonoBehaviour
 {
     private const int possibleKeyPositionsCount = 10;
     private readonly Vector2[] possibleKeyPositions = {
-        new Vector2(-15f, 20f),
-        new Vector2(-1f, 26.5f),
-        new Vector2(6.5f, 28f),
-        new Vector2(-13.5f, 10f),
-        new Vector2(-12f, 6f),
-        new Vector2(9f, 5.5f),
-        new Vector2(7f, 0f),
-        new Vector2(23f, 1.5f),
-        new Vector2(-57f, -5f),
-        new Vector2(35f, 1.5f)
+        new(-15f, 20f),
+        new(-1f, 26.5f),
+        new(6.5f, 28f),
+        new(-13.5f, 10f),
+        new(-12f, 6f),
+        new(9f, 5.5f),
+        new(7f, 0f),
+        new(23f, 1.5f),
+        new(-57f, -5f),
+        new(35f, 1.5f)
     };
 
     [Range(1, possibleKeyPositionsCount)]
     public int keyCount = 3;
 
+    List<Vector2> keyPositions = new();
+
     void Start()
     {
-        
+        for (int i = 0; i < keyCount; i++)
+        {
+            int randomIndex = Random.Range(0, possibleKeyPositionsCount);
+            keyPositions.Add(possibleKeyPositions[randomIndex]);
+        }
+
+        GameObject keyPrefab = Resources.Load<GameObject>("Prefabs/Key");
+        foreach (Vector2 position in keyPositions)
+        {
+            GameObject key = Instantiate(keyPrefab, position, Quaternion.identity);
+            key.transform.SetParent(transform, worldPositionStays: false);
+        }
+
+        Destroy(this);
     }
 
     void Update()
